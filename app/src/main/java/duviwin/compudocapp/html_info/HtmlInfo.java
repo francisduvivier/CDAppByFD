19486ac8b81e64f4021e7ff0ed8a0a6a2b58b9ed
Francis Duvivier <francisduvivier@gmail.com>
2015-07-09 21:19:24 +0200

    htmlinfo erbij nogns

--- app/src/main/java/duviwin/compudocapp/html_info/HtmlInfo.java
+++ app/src/main/java/duviwin/compudocapp/html_info/HtmlInfo.java
@@ -1,7 +1,12 @@
 package duviwin.compudocapp.html_info;
 
+import java.io.Serializable;
 /**
- * Created by Duviwin on 7/9/2015.
+ * Created by Duviwin on 7/8/2015.
  */
-public class HtmlInfo {
+public interface HtmlInfo extends Serializable {
+    String getPattern();
+    HtmlInfoEnum[] getVals();
+    int getOpdrNrIndex();
 }
+
